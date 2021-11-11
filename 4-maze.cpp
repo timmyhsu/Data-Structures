d278591c6f17120ecd8a9247c397b54a1f637b0e
LS <43104666+timmyhsu@users.noreply.github.com>
2021-11-11 13:44:44 +0800

    Update 4-maze.cpp

--- 4-maze.cpp
+++ 4-maze.cpp
@@ -1,3 +1,4 @@
+                                                                                                                                                         1 ⚙
 #include <iostream>
 #include "queue.cpp"
 #include<stack>
@@ -23,11 +24,11 @@ int main()
         for (int x = 0 ; x<height + 2 ; ++x )
                 maze[x] = new int [width + 2] ;
 
-        for(int x =0 ; x<height+2 ;++x) // first , let all of them is N 
+        for(int x =0 ; x<height+2 ;++x) // first , let all of them is -1
         {
                 for (int y = 0;y<width+2;++y)
                 {
-                        maze[x][y] = N ;
+                        maze[x][y] = -1 ;
                 }
         }
 
@@ -35,7 +36,7 @@ int main()
         {
                 for(int y= 1;y<=width;++y)
                 {
-                        maze[x][y] = -1 ; // second , let the variable in [1 -> height][1 -> width] be -1 , see it as the path doesn't walk yet 
+                        maze[x][y] = N ; // second , let the variable in [1 -> height][1 -> width] be N , see it as the path doesn't walk yet 
                 }
         }
         node start ,tail ;
@@ -46,7 +47,7 @@ int main()
                 while (cin >> fir && fir != 0 ) // input the first number 
                 {
                         cin >> sec ; // input the second number and identify it is 'x' ,'s' ,or 't'
-                        if (sec == 'x') maze[x][fir] = N ;
+                        if (sec == 'x') maze[x][fir] = -1 ;
                         else if (sec == 's') 
                         {
                                 start.x = x ; start.y = fir ;start.step = 0 ;
@@ -62,25 +63,25 @@ int main()
 
         while (!((path.front().x == tail.x) && (path.front().y == tail.y))) //until the front of queue is equal to the tail 
         {
-                if ( maze[path.front().x][path.front().y-1] != N && maze[path.front().x][path.front().y-1] == -1) // left
+                if ( maze[path.front().x][path.front().y-1] > maze[path.front().x][path.front().y] && maze[path.front().x][path.front().y-1] != -1) // left
                 {
                         node temp (path.front().x ,path.front().y-1,path.front().step+1) ;
                         path.push(temp) ;
                         maze[path.front().x][path.front().y-1] = path.front().step + 1 ;
                 }
-                if ( maze[path.front().x][path.front().y+1] != N && maze[path.front().x][path.front().y+1] == -1) // right
+                if ( maze[path.front().x][path.front().y+1] > maze[path.front().x][path.front().y] && maze[path.front().x][path.front().y+1] != -1) // right
                 {
                         node temp (path.front().x ,path.front().y+1,path.front().step+1) ;
                         path.push(temp) ;
                         maze[path.front().x][path.front().y+1] = path.front().step + 1 ;
                 }
-                if ( maze[path.front().x-1][path.front().y] != N && maze[path.front().x-1][path.front().y] == -1) // up
+                if ( maze[path.front().x-1][path.front().y] > maze[path.front().x][path.front().y] && maze[path.front().x-1][path.front().y] != -1) // up
                 {
                         node temp (path.front().x-1 ,path.front().y,path.front().step+1) ;
                         path.push(temp) ;
                         maze[path.front().x-1][path.front().y] = path.front().step + 1 ;
                 }
-                if ( maze[path.front().x+1][path.front().y] != N && maze[path.front().x+1][path.front().y] == -1) // down
+                if ( maze[path.front().x+1][path.front().y] > maze[path.front().x][path.front().y] && maze[path.front().x+1][path.front().y] != -1) // down
                 {
                         node temp (path.front().x+1 ,path.front().y,path.front().step+1) ;
                         path.push(temp) ;
@@ -89,9 +90,10 @@ int main()
 
                 path.pop();
         }
+        // =========the output of the path of the maze ========
         tail = path.front() ; 
         int x = tail.x ,y = tail.y ;
-        node arr [tail.step+1] = {} ;
+        node arr [tail.step+1] = {} ; // this array is to store the step of the shortest path 
         arr[tail.step] = tail ;
         for (int i = tail.step -1 ; i >=0 ; --i) // this for loop is to store the route from the last point to the start point
         {
@@ -122,5 +124,39 @@ int main()
                 cout << "(" << arr[i].x << "," << arr[i].y << ")" << endl ;
         }
 
+
+        // ===============output the maze ===================
+        int maze_t[height+2][width+2] ; // temporary maze 
+        for (int x =0 ;x< height+2 ;++x ) // initialize the maze all to 0 
+        {
+                for( int y = 0 ;y < width+2 ; ++y )
+                        maze_t[x][y] = 0 ;
+        }
+
+        for (int x =0 ;x< height+2 ;++x ) // let the boundary and th e wall be -1 
+        {
+                for( int y = 0 ;y < width+2 ; ++y )
+                        if ( maze[x][y] == -1 ) maze_t[x][y] = -1 ;
+        }
+
+        for (int i = 0 ; i<=tail.step ;++i) //output the array that store the shortest path of the maze 
+        {
+                maze_t[arr[i].x][arr[i].y] = i ;
+        }
+
+        for (int x =0 ;x < height +2 ; ++x ) // output the maze 
+        {
+                for (int y = 0; y < width +2 ;++y )
+                {
+                        if ( maze_t[x][y] == -1) cout << "x " ;
+                        else cout << maze_t[x][y] << " " ;
+                }
+                cout << endl ;
+        }
+
+        // ==============delete the maze ===============
+        for (int x = 0 ;x < height +2 ; ++x )
+                delete [] maze[x] ;
+        delete [] maze ;
 
 }
